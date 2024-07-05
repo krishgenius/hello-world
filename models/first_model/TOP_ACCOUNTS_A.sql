f000499b151445c89f686e3a330bef96aadfe29b
knagarajan@salesforce.com <knagarajan@salesforce.com>
2024-07-05 19:39:12 +0000

    c

--- models/first_model/TOP_ACCOUNTS_A.sql
+++ models/first_model/TOP_ACCOUNTS_A.sql
@@ -1,4 +1,4 @@
 {{config(materialized='table')}}
-select id, name 
-from {{ref("TOP_ACCOUNTS")}}
-where upper(name) like 'A%'
+select acct_id, acct_nm 
+from edw_dm_dev.common.dim_account--{{ref("TOP_ACCOUNTS")}}
+where upper(acct_nm) like 'A%'

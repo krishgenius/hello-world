bb77a9ae23265e89f97e126fe6e9754da01af7a9
knagarajan@salesforce.com <knagarajan@salesforce.com>
2024-07-08 21:56:16 +0000

    change

--- models/POC/dim_opportunity_poc.sql
+++ models/POC/dim_opportunity_poc.sql
@@ -5,32 +5,11 @@
 
 with source_data as (
     select
-        *
-    from edw_ods_dev.org62.ods_opportunity
+        *, current_timestamp as dbt_updated_dt
+    from edw_ods_dev.org62.ods_opportunity)
+    select * from source_data 
     {% if is_incremental() %}
-    where audit_etl_job_upd_ts > (select max(audit_etl_job_upd_ts) from {{ this }})
+    where audit_etl_job_upd_ts > (select max({{ this }}.dbt_updated_dt) from {{ this }})
     {% endif %}
-)
 
-{% if is_incremental() %}
--- Step 1: Delete matching records from the target table
 
-merge into {{ this }} as target
-using source_data as source
-on target.id = source.id
-when matched then
-    update set
-        name = source.name,
-        audit_etl_job_upd_ts = current_timestamp()
-when not matched then
-    insert (id, name, audit_etl_job_upd_ts)
-    values (source.id, source.name, current_timestamp());
-
-{% else %}
-
--- Full refresh logic (initial load)
-select
-  *
-from source_data
-
-{% endif %}

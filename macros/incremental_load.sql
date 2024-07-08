e4bc9d4739c01bc6405d17e890a51c5dfbad624f
knagarajan@salesforce.com <knagarajan@salesforce.com>
2024-07-08 23:38:35 +0000

    c

--- macros/incremental_load.sql
+++ macros/incremental_load.sql
@@ -1,16 +1,16 @@
 {% macro incremental_load(source_table, target_table, unique_key, updated_at) %}
 
-{{ config(
-    materialized='incremental',
-    unique_key='id'
-) }}
+--{{ config(
+ --   materialized='incremental',
+   -- unique_key='id'
+--) }}
 
 with source_data as (
     select *, current_timestamp() as dbt_updated_dt
     from {{ source_table }}
 ) 
     select * from source_data
-    {% if is_incremental() %}
+        {% if is_incremental() %}
     where audit_etl_job_upd_ts > (select NVL(max({{ this }}.dbt_updated_dt),'1-JAN-1900') from {{ this }})
     {% endif %}
 

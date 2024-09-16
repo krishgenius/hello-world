68846a11dc752ffbf13ade2d2eacfb34081cdf96
knagarajan@salesforce.com <knagarajan@salesforce.com>
2024-09-16 17:53:43 +0000

    first save - traning branch

--- models/first_model/TOP_ACCOUNTS.sql
+++ models/first_model/TOP_ACCOUNTS.sql
@@ -1,5 +1,5 @@
 {{config(materialized='table')}}
-with acct_cols as (select acct_id, acct_nm from edw_dm_dev.common.dim_account)
+with acct_cols as  (select acct_id, acct_nm from edw_dm_dev.common.dim_account)
 select *
 from acct_cols
 where length(acct_nm) > 20

d132d0687267843b2fc9b66fc1d0c03af64f86fd
knagarajan@salesforce.com <knagarajan@salesforce.com>
2024-09-25 18:14:27 +0000

    del

--- models/marts/sample_dia.sql
+++ models/marts/sample_dia.sql
@@ -5,7 +5,8 @@ warehouse as (select * from {{ ref('stg_tcps__warehouse') }}),
 -- TRANSFORM
 transform as (
 SELECT
-    hash(w.w_warehouse_name||i.i_category)  as skey,
+    {{ dbt_utils.generate_surrogate_key(['w.w_warehouse_name', 'i.i_category']) }} as skey, 
+    --hash(w.w_warehouse_name||i.i_category)  as skey,
      w.w_warehouse_name,
     i.i_category,
     SUM(iv.inv_quantity_on_hand) AS total_stock
@@ -14,7 +15,7 @@ inventory iv
 JOIN item  i ON iv.inv_item_sk = i.i_item_sk
 JOIN warehouse w ON iv.inv_warehouse_sk = w.w_warehouse_sk
 GROUP BY w.w_warehouse_name, i.i_category,
-   hash(w.w_warehouse_name||i.i_category)
+  {{ dbt_utils.generate_surrogate_key(['w.w_warehouse_name', 'i.i_category']) }}
 )
 -- FINAL SELECT
 select * from transform
